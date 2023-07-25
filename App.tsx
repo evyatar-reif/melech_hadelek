31dde2c6f552ae3068e18ec108c4967cb860c9a6
evyatar <evyatar.reif@gmail.com>
2023-07-25 14:15:40 +0300

    redux persist

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import HomeScreen from './screens/HomeScreen/HomeScreen';
-import CarPicker from './screens/CarPicker';
 import {Provider} from 'react-redux';
-import {store} from './redux/store';
+import {PersistGate} from 'redux-persist/es/integration/react';
+import {store, persistor} from './redux/store';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
@@ -10,15 +10,16 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false,
-          }}>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="CarPicker" component={CarPicker} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false,
+            }}>
+            <Stack.Screen name="Home" component={HomeScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 }
--- redux/store.js
+++ redux/store.js
@@ -1,24 +1,23 @@
 import settingsReducer from './settingsReducer';
 import {configureStore, combineReducers} from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
+import AsyncStorage from '@react-native-community/async-storage';
+import {persistReducer, persistStore} from 'redux-persist';
 
-// import storage from 'redux-persist/lib/storage';
-// import { persistReducer, persistStore } from 'redux-persist';
-
-// const persistConfig = {
-//     key: 'root',
-//     storage,
-// };
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
 
 const rootReducer = combineReducers({
   settingsState: settingsReducer,
 });
 
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: [thunk],
 });
 
-// export const persistor = persistStore(store);
+export const persistor = persistStore(store);
